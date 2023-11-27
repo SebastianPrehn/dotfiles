426679eff85d26308d1d893fe2d3e48bfa7c25c7
Sebastian Larsen Prehn <slp@di.ku.dk>
2023-11-27 12:22:43 +0100

    WIP install script and a reminder added to emacs init file

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -9,6 +9,10 @@
 ;; This file bootstraps my configuration, with much of the configuration divided out
 ;; into other files as an attempt to make my configuration more "modular".
 
+;; TODO make more packages deferred in order to speed up load and minimize mindless loading.
+;; Etc. will sadly not be using Haskell for work most likely, thus an easy to make sure only
+;; loads when not on work computer
+
 ;;; Code:
 
 ;; Produce backtraces when errors occur: can be helpful to diagnose startup issues
---
+++ install-macos.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# Install Brew
+# NONINTERACTIVE=1 /bin/zsh -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+# TODO Add all programs needed to support Emacs and that I always use
+brew install \
+     git \
+     ispell \
+     vale
