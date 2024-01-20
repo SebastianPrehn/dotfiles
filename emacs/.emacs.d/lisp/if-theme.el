44fcbe411d35e6ceb41f2b5c2b99a5bbcd5da868
Sebastian Larsen Prehn <sbstprehn@icloud.com>
2024-01-20 23:55:21 +0100

    Changed to Nord theme and fixed a spelling mistake for bold

--- emacs/.emacs.d/lisp/if-theme.el
+++ emacs/.emacs.d/lisp/if-theme.el
@@ -5,19 +5,23 @@
 
 (use-package solaire-mode
   :config
-  (solaire-global-mode +1))
+  (solaire-global-mode t))
 
 (use-package doom-themes
   :config
   ;; Global settings
-  (setq doom-themes-enable-bolt t ; if nil, bold is universally disabled
+  (setq doom-themes-enable-bold t ; if nil, bold is universally disabled
         doom-themes-enableitalic t)
 
-  (load-theme 'doom-wilmersdorf t)
   ;; Enable flashing mode-line on errors
   (doom-themes-visual-bell-config)
   ;; Corrects (and improves) Org-mode's native fontification
   (doom-themes-org-config))
+
+
+(use-package nord-theme
+  :config
+  (load-theme 'nord t))
 
 (provide 'if-theme)
 ;;; if-theme.el ends here
