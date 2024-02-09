1b0d8ae6836b10f6f03b9abffaea2b7415f545f4
Sebastian Larsen Prehn <sbstprehn@icloud.com>
2024-02-09 10:47:05 +0100

    Add Nix package for Emacs

---
+++ emacs/.emacs.d/lisp/dev-nix.el
@@ -0,0 +1,10 @@
+;;; dev-nix.el --- Nix setup within Emacs -*- lexical-binding: t -*-
+;;; Commentary:
+
+;;; Code:
+
+(use-package nix-mode
+  :mode "\\.nix\\'")
+
+(provide 'dev-nix)
+;;; dev-nix.el ends here
