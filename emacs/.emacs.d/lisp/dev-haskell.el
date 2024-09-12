a05fc6b5b59a4972c2ac044950ad94bf3429da0f
Sebastian Larsen Prehn <sbstprehn@icloud.com>
2024-09-12 19:34:56 +0200

    haskell-mode + ormolu setup

--- emacs/.emacs.d/lisp/dev-haskell.el
+++ emacs/.emacs.d/lisp/dev-haskell.el
@@ -1,26 +1,21 @@
 ;;; dev-haskell.el --- Configuration for Haskell -*- lexical-binding: t -*-
 ;;; Commentary:
 
-;; http://h2.jaguarpaw.co.uk/posts/how-i-use-dante/
-
 ;;; Code:
 
 (use-package haskell-mode
   :hook (haskell-mode . (lambda ()
                           (haskell-doc-mode)
-                          (turn-on-haskell-indent))))
-
-(use-package attrap)
-
-(use-package dante
-  :after haskell-mode
-  :commands 'dante-mode
-  :init
-  (add-hook 'haskell-mode-hook 'flycheck-mode)
-  (add-hook 'haskell-mode-hook 'dante-mode)
+                          (turn-on-haskell-indent)))
   :config
-  (flycheck-add-next-checker 'haskell-dante '(info . haskell-hlint))
+  (define-key haskell-mode-map (kbd "<f8>") 'haskell-navigate-imports)
+  '(haskell-process-auto-import-loaded-modules t)
+  '(haskell-process-log t)
+  '(haskell-process-suggest-remove-import-lines t)
   )
+
+(use-package ormolu
+  :hook (haskell-mode . ormolu-format-on-save-mode))
 
 (provide 'dev-haskell)
 ;;; dev-haskell.el ends here
