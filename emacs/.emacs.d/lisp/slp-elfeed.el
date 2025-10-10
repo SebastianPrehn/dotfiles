fc0b9fe9a4cc47b3d82aa8eeeefb55ccabd9786b
Sebastian Larsen Prehn <sbstprehn@icloud.com>
2025-10-10 11:45:41 +0200

    elfeed: Add a few more blogs

--- emacs/.emacs.d/lisp/slp-elfeed.el
+++ emacs/.emacs.d/lisp/slp-elfeed.el
@@ -10,7 +10,10 @@
         "https://planet.emacslife.com/atom.xml"
         "https://stallman.org/rss/rss.xml"
         "https://sophiebos.io/index.xml"
-        "https://sigkill.dk/atom.xml")))
+        "https://sigkill.dk/atom.xml"
+        "https://xkcd.com/rss.xml"
+        "https://danluu.com/atom.xml"
+        "https://drewdevault.com/blog/index.xml")))
 
 (provide 'slp-elfeed)
 ;;; slp-elfeed.el ends here
