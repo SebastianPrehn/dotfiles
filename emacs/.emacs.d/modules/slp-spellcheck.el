ea12888d2f0aae37bbcbbe852457167b5f93fe3d
Sebastian Larsen Prehn <slp@sebastianprehn.dk>
2026-02-01 17:45:40 +0100

    Add: Vale as a grammar tool

--- emacs/.emacs.d/modules/slp-spellcheck.el
+++ emacs/.emacs.d/modules/slp-spellcheck.el
@@ -4,4 +4,19 @@
   :bind (("M-$" . jinx-correct)
          ("C-M-$" . jing-languages)))
 
+(use-package flycheck
+  :ensure t
+  :hook ((text-mode org-mode markdown-mode) . flycheck-mode))
+
+;; Custom Vale checker instead of flycheck-vale package
+(flycheck-define-checker vale
+  "A checker for prose using Vale"
+  :command ("vale" "--output" "line" source)
+  :standard-input nil
+  :error-patterns
+  ((error line-start (file-name) ":" line ":" column ":" (id (one-or-more (not (any ":")))) ":" (message) line-end))
+  :modes (markdown-mode org-mode text-mode))
+
+(add-to-list 'flycheck-checkers 'vale 'append)
+
 (provide 'slp-spellcheck)
